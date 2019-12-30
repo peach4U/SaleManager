4328c92124ed46f7c7170524fd4cb4d550e2d5f6
peach4U <59299779+peach4U@users.noreply.github.com>
2019-12-30 11:11:39 +0800

    Add files via upload

--- SaleService.cs
+++ SaleService.cs
@@ -11,13 +11,13 @@ namespace EmployeeManager
         public override string CalcuteMoney(Employee user)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("【计算公式】销售数量*每个商品单价;\r\n");
+            sb.Append("【计算公式】销售数量*每个商品提成+打底工资;\r\n");
             sb.Append("编号:" + user.Number + "\r\n");
             sb.Append("姓名:" + user.Name + "\r\n");
             sb.Append("类型:" + user.UserType + "\r\n");
             sb.Append("销售数量:" + user.SaleCount + "\r\n");
             sb.Append("个/元:" + user.SalePrice + "\r\n");
-            sb.Append("工资:" + (user.SaleCount * user.SalePrice) + "\r\n");
+            sb.Append("工资:" + (user.SaleCount * user.SalePrice) +"\r\n");
             return sb.ToString();
         }
     }
